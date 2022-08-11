b8b36db5e0179506fdb6db0207de3d728b495136
Igor <102155826+igorentiy@users.noreply.github.com>
2022-08-11 18:01:25 +0300

    Update b.py

--- python/sprint1_nonfinals/b.py
+++ python/sprint1_nonfinals/b.py
@@ -1,6 +1,8 @@
 def check_parity(a: int, b: int, c: int) -> bool:
-    # Здесь реализация вашего решения
-    pass
+    if a%2 == 0 and b%2 == 0 and c%2 == 0:
+        return True
+    elif a%2 != 0 and b%2 != 0 and c%2 != 0:
+        return True
 
 def print_result(result: bool) -> None:
     if result:
